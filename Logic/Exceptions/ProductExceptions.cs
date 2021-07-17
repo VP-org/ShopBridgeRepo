81e2f952070af08777b253fece248b3426280b03
Vish243 <33545942+EDMDesigners@users.noreply.github.com>
2021-07-17 16:54:49 +0530

    Add files via upload

---
+++ Logic/Exceptions/ProductExceptions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic.Exceptions
+{
+    public class ProductNotFoundException : Exception
+    {
+        public ProductNotFoundException() : base($"Product not found")
+        {
+            
+        }
+
+        public ProductNotFoundException(int id) : base($"Product id '{id}' not found")
+        {
+
+        }
+    }
+
+    public class ProductAlreadyExistsException : Exception
+    {
+        public ProductAlreadyExistsException() : base($"Product already exists")
+        {
+
+        }
+
+        public ProductAlreadyExistsException(string name) : base($"Product name '{name}' already exists")
+        {
+
+        }
+    }
+}
---
+++ Logic/Interfaces/IProductProvider.cs
@@ -0,0 +1,16 @@
+﻿using Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Interfaces
+{
+    public interface IProductProvider
+    {
+        Task<List<ProductModel>> GetProducts();
+        Task<ProductModel> LoadProduct(int productId);
+        Task SaveProduct(ProductModel model);
+        Task DeleteProduct(int productId);
+    }
+}
