b70c2ab5e4fdd56205c0f191322e59731124c85a
Vish243 <33545942+EDMDesigners@users.noreply.github.com>
2021-07-17 16:51:56 +0530

    Add files via upload

---
+++ DataService/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DataService
+{
+    public partial class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal? Price { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+    }
+}
---
+++ DataService/Shop_Bridge_dbContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace DataService
+{
+    public partial class Shop_Bridge_dbContext : DbContext
+    {
+        public Shop_Bridge_dbContext()
+        {
+        }
+
+        public Shop_Bridge_dbContext(DbContextOptions<Shop_Bridge_dbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product");
+
+                entity.Property(e => e.CreatedBy)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Description).IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
