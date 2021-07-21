2ec214af1638f87bbbe59fb287565f0feb503375
Vish243 <33545942+EDMDesigners@users.noreply.github.com>
2021-07-21 14:29:48 +0530

    Add files via upload

---
+++ Logic/Utils/Projection.cs
@@ -0,0 +1,27 @@
+﻿using DataService;
+using Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logic.Utils
+{
+    public static class Projection
+    {
+        public static IQueryable<ProductModel> Project(this IQueryable<Product> query)
+        {
+            return query.Select(a => new ProductModel
+            {
+                Id = a.Id,
+                Name = a.Name,
+                Description = a.Description,
+                Price = a.Price,
+                CreatedBy=a.CreatedBy,
+                CreatedDate=a.CreatedDate
+
+            }).AsQueryable();
+        }
+
+    }
+}
