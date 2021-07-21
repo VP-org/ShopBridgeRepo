03c5456431369c5731a6a0e734baf551c53731b5
Vish243 <33545942+EDMDesigners@users.noreply.github.com>
2021-07-21 13:13:42 +0530

    Update ProductControllerTest.cs

--- ApiTest/Controllers/ProductControllerTest.cs
+++ ApiTest/Controllers/ProductControllerTest.cs
@@ -1,4 +1,4 @@
-﻿using Logic.Exceptions;
+using Logic.Exceptions;
 using Logic.Interfaces;
 using Logic.Models;
 using Microsoft.AspNetCore.Http;
@@ -26,15 +26,16 @@ namespace ApiTest.Controllers
             _serviceProvider = services.BuildServiceProvider();
             _products = new List<ProductModel> {
                      new ProductModel{ Id=1,Name="Shirt",Description="Polo Shirt",Price=900 },
-                     new ProductModel{ Id=1,Name="T-Shirt",Description="Rugger T-Shirt",Price=600 },
-                     new ProductModel{ Id=1,Name="Jean",Description="Levis Jean",Price=1200 },
-                     new ProductModel{ Id=1,Name="Cap",Description="Sports Cap",Price=300 },
+                     new ProductModel{ Id=2,Name="T-Shirt",Description="Rugger T-Shirt",Price=600 },
+                     new ProductModel{ Id=3,Name="Jean",Description="Levis Jean",Price=1200 },
+                     new ProductModel{ Id=4,Name="Cap",Description="Sports Cap",Price=300 },
            };
 
             Mock<IProductProvider> provider = new Mock<IProductProvider>();
-            provider.Setup(k => k.GetProducts()).ReturnsAsync(_products);
+            provider.Setup(k => k.GetProducts(It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(_products);
             provider.Setup(k => k.LoadProduct(It.IsAny<int>())).ReturnsAsync(_products[0]);
-            provider.Setup(k => k.SaveProduct(It.IsAny<ProductModel>())).Returns(Task.FromResult(false));
+            provider.Setup(k => k.SaveProduct(It.IsAny<ProductModel>())).Returns(Task.FromResult(true));
+            provider.Setup(k => k.DeleteProduct(It.IsAny<int>())).Returns(Task.FromResult(true));
             _controller = new ProductController(new Lazy<IProductProvider>(() => provider.Object));
             SetUpController(_controller);
         }
@@ -48,7 +49,8 @@ namespace ApiTest.Controllers
         [Test]
         public async Task GetProductsValid()
         {
-            GetProductsResponse response = await _controller.GetProducts();
+            GetProductsRequest data = new GetProductsRequest() { pageNumber = 2, pageSize = 2 };
+            GetProductsResponse response = await _controller.GetProducts(data);
             Assert.IsTrue(response.Success);
             Assert.AreEqual(_products.Count, response.productModels.Count);
         }
@@ -108,6 +110,41 @@ namespace ApiTest.Controllers
             var response = await _controller.SaveProduct(request);
             Assert.IsFalse(response.Success);
             Assert.AreEqual("Invalid data. Please try again", response.Message);
+        }
+
+        [Test]
+        public async Task SaveProductNotFound()
+        {
+            Mock<IProductProvider> provider = new Mock<IProductProvider>();
+            provider.Setup(k => k.SaveProduct(It.IsAny<ProductModel>())).Throws<ProductNotFoundException>();
+            var controller = new ProductController(new Lazy<IProductProvider>(() => provider.Object));
+            SetUpController(controller);
+
+            SaveProductRequest request = new SaveProductRequest();
+            request.model = new ProductModel { Id = 9, Name = "Jacket", Description = "Shirt", Price = 1500, CreatedBy = "Vish" };
+            var response = await controller.SaveProduct(request);
+            Assert.IsFalse(response.Success);
+            Assert.AreEqual("Product not found", response.Message);
+        }
+
+        [Test]
+        public async Task DeleteProductValid()
+        {
+            var response = await _controller.DeleteProduct(1);
+            Assert.IsTrue(response.Success);
+        }
+
+        [Test]
+        public async Task DeleteProductNotFound()
+        {
+            Mock<IProductProvider> provider = new Mock<IProductProvider>();
+            provider.Setup(k => k.DeleteProduct(It.IsAny<int>())).Throws<ProductNotFoundException>();
+            var controller = new ProductController(new Lazy<IProductProvider>(() => provider.Object));
+            SetUpController(controller);
+
+            var response = await controller.DeleteProduct(9);
+            Assert.IsFalse(response.Success);
+            Assert.AreEqual("Product not found", response.Message);
         }
     }
 }
